5a2b5dedef58370e17a2218b84a7d80dce585636
Sagar <sagar.0dev@gmail.com>
2024-11-09 21:42:46 +0530

    Message Border Style

--- app/src/main/java/com/sagar/fluenty/ui/screen/ConversationScreen.kt
+++ app/src/main/java/com/sagar/fluenty/ui/screen/ConversationScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -76,6 +77,7 @@ fun ConversationScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .background(Color.Black)
+                .statusBarsPadding()
                 .padding(20.dp),
             verticalArrangement = Arrangement.Bottom
         ) {
@@ -225,7 +227,7 @@ private fun AssistantMessage(message: String) {
         ) {
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(10.dp))
+                    .clip(RoundedCornerShape(topEnd = 10.dp,bottomEnd = 10.dp, bottomStart = 10.dp))
                     .animateContentSize()
                     .background(Color.DarkGray),
                 contentAlignment = Alignment.Center
@@ -257,7 +259,7 @@ private fun UserMessage(message: String) {
         ) {
             Box(
                 modifier = Modifier
-                    .clip(RoundedCornerShape(10.dp))
+                    .clip(RoundedCornerShape(topStart = 10.dp,bottomEnd = 10.dp, bottomStart = 10.dp))
                     .animateContentSize()
                     .background(Color.DarkGray),
                 contentAlignment = Alignment.Center
@@ -268,6 +270,5 @@ private fun UserMessage(message: String) {
                 )
             }
         }
-
     }
 }
