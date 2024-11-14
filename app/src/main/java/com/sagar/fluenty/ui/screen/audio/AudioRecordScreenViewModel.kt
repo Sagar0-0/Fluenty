08fbc5861e6a6bac1b0332980ac4a66df7f9ca30
Sagar <sagar.0dev@gmail.com>
2024-11-14 23:25:24 +0530

    Updated Audio Prompt

--- app/src/main/java/com/sagar/fluenty/ui/screen/audio/AudioRecordScreenViewModel.kt
+++ app/src/main/java/com/sagar/fluenty/ui/screen/audio/AudioRecordScreenViewModel.kt
@@ -57,7 +57,7 @@ class AudioRecordScreenViewModel(
         audioPlayerManager.initListener(this)
 
         getResponse(
-            "You have to act as an English teacher and have to teach me english. More specifically, you have to start by giving me a English word that I have to pronounce, record and send you in an audio file. Then you have to understand the word said in audio file and tell me if the pronunciation of the word is correct or not(do not give long responses). Make sure to tell me the actual pronunciation in American accent. And also score me from 0 to 100 on how much accurate was my pronunciation, if the score is above 80, give next word. Make sure to stick to this context. Now, start by giving me a simple word."
+            "You have to act as an English teacher and have to teach me english. More specifically, you have to start by giving me a English word that I have to pronounce, record and send you in an audio file. Then you have to understand the word said in audio file and tell me if the pronunciation of the word is correct or not(do not give long responses). Make sure to tell me the actual pronunciation in American accent with phoneme breakdown of the word(like *kuhmf-tr-bl* for comfortable). And also score me from 0 to 100 on how much accurate was my pronunciation, if the score is above 80, give next word. Make sure to stick to this context. Now, start by giving me a simple word."
         )
     }
 
